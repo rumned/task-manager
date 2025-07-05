d76f37cf726e87482b2e2cb7ec6bb2364737240d
Ramdzuanny <rumnedmus@gmail.com>
2025-07-05 11:51:26 +0800

    *

--- client/src/components/AddTaskForm.jsx
+++ client/src/components/AddTaskForm.jsx
@@ -2,43 +2,44 @@ import { Box, TextField, Button } from "@mui/material";
 import { useState } from "react";
 import { addTask } from "../api/tasks";
 function AddTaskForm({ fetchData }) {
-	const [task, setTask] = useState("");
+  const [task, setTask] = useState("");
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		if (!task.trim()) return;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!task.trim()) return;
 
-		let { data } = await addTask({ description: task });
-		alert(data.msg);
-		fetchData();
-	};
+    let { data } = await addTask({ description: task });
+    alert(data.msg);
+    fetchData();
+  };
 
-	return (
-		<Box
-			component="form"
-			onSubmit={handleSubmit}
-			sx={{
-				display: "flex",
-				flexDirection: { xs: "column", sm: "row" },
-				alignItems: "center",
-				justifyContent: "center",
-				gap: 2,
-				width: "90%",
-				maxWidth: "600px",
-				margin: "20px auto",
-			}}>
-			<TextField
-				label="Add new task"
-				variant="outlined"
-				fullWidth
-				value={task}
-				onChange={(e) => setTask(e.target.value)}
-			/>
-			<Button variant="contained" color="primary" type="submit">
-				Add
-			</Button>
-		</Box>
-	);
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 2,
+        width: "90%",
+        maxWidth: "600px",
+        margin: "20px auto",
+      }}
+    >
+      <TextField
+        label="Add new task"
+        variant="outlined"
+        fullWidth
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Add
+      </Button>
+    </Box>
+  );
 }
 
 export default AddTaskForm;
--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import {
-	Grid,
-	Card,
-	CardContent,
-	Typography,
-	Button,
-	Box,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  Box,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CheckIcon from "@mui/icons-material/Check";
@@ -13,85 +13,85 @@ import EditIcon from "@mui/icons-material/Edit";
 import { onComplete, deleteTask } from "../api/tasks";
 
 import EditTaskForm from "./EditTaskForm";
+import { lightBlue } from "@mui/material/colors";
 function TaskItem({ task, fetchData }) {
-	const [editing, setEditing] = useState(false);
-	const deleteHandler = async (taskId) => {
-		if (window.confirm("Are you sure you want to delete this task?")) {
-			let data = await deleteTask(taskId);
-			if (data.success) {
-				alert(data.msg);
-				fetchData();
-			}
-		} else {
-			return;
-		}
-	};
-	return (
-		<Grid size={{ lg: 4, md: 6, xs: 12 }}>
-			<Card style={{ marginBottom: "5px" }}>
-				<CardContent>
-					{editing ? (
-						<EditTaskForm
-							task={task}
-							setEditing={setEditing}
-							fetchData={fetchData}
-						/>
-					) : (
-						<>
-							<Typography
-								variant="h6"
-								sx={{
-									textDecoration: task.completed
-										? "line-through"
-										: "none",
-									color: task.completed ? "gray" : "inherit",
-								}}>
-								{task.description}
-							</Typography>
+  const [editing, setEditing] = useState(false);
+  const deleteHandler = async (taskId) => {
+    if (window.confirm("Are you sure you want to delete this task?")) {
+      let data = await deleteTask(taskId);
+      if (data.success) {
+        alert(data.msg);
+        fetchData();
+      }
+    } else {
+      return;
+    }
+  };
+  return (
+    <Grid size={{ lg: 4, md: 6, xs: 12 }}>
+      <Card style={{ marginBottom: "5px" }}>
+        <CardContent>
+          {editing ? (
+            <EditTaskForm
+              task={task}
+              setEditing={setEditing}
+              fetchData={fetchData}
+            />
+          ) : (
+            <>
+              <Typography
+                variant="h6"
+                sx={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                  color: task.completed ? "gray" : "inherit",
+                }}
+              >
+                {task.description}
+              </Typography>
 
-							<Box
-								sx={{
-									display: "flex",
-									justifyContent: "space-between",
-									mt: 2,
-								}}>
-								<Button
-									variant="contained"
-									color="success"
-									disabled={task.completed}
-									onClick={async () => {
-										await onComplete(task._id);
-										await fetchData();
-									}}>
-									{task.completed ? (
-										"Completed"
-									) : (
-										<CheckIcon />
-									)}
-								</Button>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  mt: 2,
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="success"
+                  disabled={task.completed}
+                  onClick={async () => {
+                    await onComplete(task._id);
+                    await fetchData();
+                  }}
+                >
+                  {task.completed ? "Completed" : <CheckIcon />}
+                </Button>
 
-								{!task.completed ? (
-									<Button
-										color="warning"
-										variant="outlined"
-										onClick={() => setEditing(true)}>
-										<EditIcon />
-									</Button>
-								) : null}
+                {!task.completed ? (
+                  <Button
+                    color="warning"
+                    variant="outlined"
+                    onClick={() => setEditing(true)}
+                  >
+                    <EditIcon />
+                  </Button>
+                ) : null}
 
-								<Button
-									color="error"
-									variant="contained"
-									onClick={() => deleteHandler(task._id)}>
-									<DeleteIcon />
-								</Button>
-							</Box>
-						</>
-					)}
-				</CardContent>
-			</Card>
-		</Grid>
-	);
+                <Button
+                  color="error"
+                  variant="contained"
+                  onClick={() => deleteHandler(task._id)}
+                >
+                  <DeleteIcon />
+                </Button>
+              </Box>
+            </>
+          )}
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }
 
 export default TaskItem;
